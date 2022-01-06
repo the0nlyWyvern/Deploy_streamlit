b2933c154510535cd87c43e2ef477125959c0258
thanh khiet <cs.thanhkhiet@gmail.com>
2022-01-06 22:50:59 +0700

    change text_area

--- Deploy.py
+++ Deploy.py
@@ -56,8 +56,8 @@ def tokennize_text(X_df):
     Tokenize the sentences into words
     return a numpy array of preprocessed text, each item in array is a paragraph after remove stopwords, special characters, ...
     '''
-    annotator = VnCoreNLP(address="http://127.0.0.1", port=9000)
-    #annotator = VnCoreNLP("VnCoreNLP/VnCoreNLP-1.1.1.jar")
+    # annotator = VnCoreNLP(address="http://127.0.0.1", port=9000)
+    annotator = VnCoreNLP("VnCoreNLP/VnCoreNLP-1.1.1.jar")
     with annotator:
         for index, filtered_text in enumerate(X_df):
             token_text = annotator.tokenize(filtered_text)[0]
@@ -116,7 +116,7 @@ def main():
     st.title('VIETNAMESE FAKE NEWS DETECTION')
     st.text(authors)
 
-    news = st.text_input('Write your news here')
+    news = st.text_area('Write your news here')
 
     model = st.selectbox('Select your model',options=['Logistic Regression','Decision Tree',
                                                      'Gradient Booting Classifier','Linear Regession',
